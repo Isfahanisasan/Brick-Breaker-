097d449550c47e899d664639a273b4481b6e06f9
hcsong213 <51963099+hcsong213@users.noreply.github.com>
2023-02-28 17:31:19 -0800

    Boilerplate Hotfix

--- brick-breaker.js
+++ brick-breaker.js
@@ -35,7 +35,19 @@ export class BrickBreaker extends Scene {
         this.key_triggered_button("Attach to moon", ["Control", "m"], () => this.attached = () => this.moon);
     }
 
+
     display(context, program_state) {
+        if (!context.scratchpad.controls) {
+            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            // Define the global camera and projection matrices, which are stored in program_state.
+            program_state.set_camera(this.initial_camera_location);
+        }
+
+        program_state.projection_transform = Mat4.perspective(
+            Math.PI / 4, context.width / context.height, .1, 1000);
+
+
+        const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
 
     }
 }
